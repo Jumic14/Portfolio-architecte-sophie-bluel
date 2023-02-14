37711efbe5be6e3292fb5943fdfa13642431d8e0
Julien <michel-julien1@hotmail.fr>
2023-02-14 11:25:32 +0100

    Mise en place des filtres dynamiques dans script.js et style.css

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,48 +1,141 @@
-// On enregistre l'URL de l'API
+// On enregistre l'URL de l'API et les containers "gallery" et "portfolio"
 const urlApi = "http://localhost:5678/api";
 
-// On enregistre le container
 const divGallery = document.querySelector(".gallery");
 
-// On récupère les données de l'API (objet Promise en Json)
+const sectionPortfolio = document.querySelector("#portfolio")
+
 /**
+ * On récupère les projets de l'API 
  * @returns {Promise<Json>}
  */
 
 const getProjects = async () => {
     let res = await fetch(urlApi + "/works");
-    let jsonListProject = await res.json();
-    return jsonListProject;
+    let projects = await res.json();
+    return projects;
 }
 
-
-// On ajoute les données à la page web
+// Affichage dynamique des projets
 /**
+ * On crée les éléments HTML des projets et on les ajoute
  * @param {JSON} projects
  */
-
-const addProjects = (projects) => {                         
+function addProjects(projects) {                        
     for (let project of projects) {
-        let newFigure = document.createElement("figure");       // On crée une nouvelle balise "figure"
-        divGallery.appendChild(newFigure);                      // On la rattache au container
-        let newImage = document.createElement("img");           // On crée une nouvelle balise "img"
-        newImage.src = project.imageUrl;                        // On lui affecte l'Url de l'img récupérée dans l'API
-        let newTitle = document.createElement("figcaption");    // On crée une nouvelle balise "figcaption"
-        newTitle.innerText = project.title;                     // On lui affecte le titre récupéré dans l'API
-        newFigure.append(newImage, newTitle);                   // On rattache les balises "img" et "figcaption" à la balise "figure"
+        let newFigure = document.createElement("figure");       
+        newFigure.setAttribute("id", project.category.name)
+        divGallery.appendChild(newFigure);                     
+        let newImage = document.createElement("img");          
+        newImage.src = project.imageUrl;                        
+        let newTitle = document.createElement("figcaption");    
+        newTitle.innerText = project.title;                     
+        newFigure.append(newImage, newTitle);                   
     }
 }
 
-// On vide le container :
-// Tant que le container contient un élément firstChild, le container supprime le premier élément Child
+// On vide le container "gallery"
 const emptyGallery = () => {
     while (divGallery.firstChild) {
     divGallery.removeChild(divGallery.firstChild);
     }
 }
 
-// On génère l'affichage dynamique de la page
-function generateProjects() {
-    emptyGallery();
-    getProjects().then(json => addProjects(json));
+// Affichage dynamique et gestion des filtres
+/**
+ * On récupère les catégories appelées par la fonction getProjects()
+ * @param {JSON} projects
+ */
+function getFilterCategories(projects) {
+    const categories = new Set();
+    for (let project of projects) {
+        const category = project.category;
+        categories.add(category.name);
+    }
+    return categories;
 }
+
+/**
+ * On crée les éléments HTML des filtres et on les ajoute
+ * @param {JSON} categories
+ */
+function addFilters(categories) {
+    let listFilters = document.createElement("div");
+    listFilters.setAttribute("class", "filters");
+    sectionPortfolio.insertBefore(listFilters, divGallery);
+    let filterAll = document.createElement("button");
+    filterAll.innerText= "Tous";
+    filterAll.setAttribute("class", "filter");
+    listFilters.appendChild(filterAll);
+    for (let category of categories) {
+        let newFilter = document.createElement("button");
+        newFilter.setAttribute("class", "filter");
+        newFilter.setAttribute("id", category);
+        newFilter.innerHTML = category;
+        listFilters.appendChild(newFilter);
+    }
+    return categories;
+}
+
+// On ajoute l'état "active" puis on l'enlève pour le filtre sélectionné
+function activeFilter (event) {
+    const currentFilter = document.querySelectorAll(".filter")
+    for (let allFilter of currentFilter) {
+        allFilter.addEventListener("click", function() {
+            for (let allFilterRemove of currentFilter) {
+                allFilterRemove.classList.remove("active")
+            }
+        allFilter.classList.add("active")
+        })
+    }
+}
+
+// On filtre les projets en fonction du filtre sélectionné
+function onButtonClick (event) {
+    let category = this.id;
+    const listProjects = document.querySelectorAll(".gallery > figure");
+        for (let listProject of listProjects) {
+            if (!category) {
+                listProject.setAttribute("style", "display:block");
+            } else if (listProject.id === category){
+                listProject.setAttribute("style", "display:block")
+            } else {
+                listProject.setAttribute("style", "display:none")
+            } 
+        }
+    
+}
+
+// On rassemble les fonctionnalités des filtres dans une fonction
+function useFilter() {
+    document.querySelectorAll(".filter").forEach(li => {
+        li.addEventListener("click", onButtonClick)
+        activeFilter();
+    })
+}
+
+// On génère l'affichage dynamique des projets
+function generateProjects() {
+    getProjects().then(json => {
+        if (json) {
+            emptyGallery();
+            addProjects(json)
+        } else {
+            console.log("Problèmes d'accès au serveur");
+        }})
+}
+   
+// On génère l'affichage dynamique des filtres et leur gestion  
+function generateFilters()  {
+    getProjects().then(projects => {
+        let categories = getFilterCategories(projects);
+        addFilters(categories);
+        useFilter(categories);
+       })
+}
+
+generateProjects();
+generateFilters();
+
+// To-Do : combiner les fonctions generateProjects() et generateFilters()
+//         -> 1 seul appel à l'API
